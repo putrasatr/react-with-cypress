e7f24d2f24d61d07ac4d80796fe8509f35e6f4b9
putrasatr <satriaputrapratama2002@gmail.com>
2022-01-31 16:24:27 +0700

    Update test components cypress

--- cypress/integration/accompliments.spec.js
+++ cypress/integration/accompliments.spec.js
@@ -5,29 +5,30 @@ describe("Accomplishment dashboard", () => {
     cy.visit("/accomplishments");
   });
 
-//   it("should display inappropriate content error when text that includes giraffe is submitted", () => {
-//     cy.get("[placeholder='Title']").type("This is my accomplishment");
-//     cy.get("[placeholder='My accomplishment...']").type("I pet a giraffe");
-//     cy.get("[type='checkbox']").click();
-//     cy.get("button").click();
-//     cy.contains("Your content is not appropriate").should("be.visible");
-//   });
+  it("should display inappropriate content error when text that includes giraffe is submitted", () => {
+    cy.get("[placeholder='Title']").type("This is my accomplishment");
+    cy.get("[placeholder='My accomplishment...']").type("I pet a giraffe");
+    cy.get("[type='checkbox']").click();
+    cy.get("button").click();
+    cy.contains("Your content is not appropriate").should("be.visible");
+  });
 
-//   it("should display inappropriate content error when text that includes giraffe is submitted (with mock)", () => {
-//     cy.intercept("POST", "http://localhost:4000", (req) => {
-//       req.reply((res) => {
-//         res.send({
-//           msg: "Your content is not appropriate",
-//         });
-//       });
-//     });
+  it("should display inappropriate content error when text that includes giraffe is submitted (with mock)", () => {
+    cy.intercept("POST", "http://localhost:4000", (req) => {
+      req.reply((res) => {
+        res.send({
+          msg: "Your content is not appropriate",
+        });
+      });
+    });
 
-//     cy.get("[placeholder='Title']").type("This is my accomplishment");
-//     cy.get("[placeholder='My accomplishment...']").type("I pet a giraffe");
-//     cy.get("[type='checkbox']").click();
-//     cy.get("button").click();
-//     cy.contains("Your content is not appropriate").should("be.visible");
-//   });
+    cy.get("[placeholder='Title']").type("This is my accomplishment");
+    cy.get("[placeholder='My accomplishment...']").type("I pet a giraffe");
+    cy.get("[type='checkbox']").click();
+    cy.get("button").click();
+    cy.contains("Your content is not appropriate").should("be.visible");
+  });
+  
   it("Ngetest aja ini", () => {
     cy.request("http://localhost:4000/rewards").as("rewards");
 
@@ -36,6 +37,5 @@ describe("Accomplishment dashboard", () => {
       expect(response).to.have.property("status", 200);
       expect(response).to.have.property("headers");
     });
-
   });
 });
