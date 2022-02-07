3e4d1f981f38e47a29240946b13eeb6b33e2caf2
putrasatr <satriaputrapratama2002@gmail.com>
2022-02-07 13:30:17 +0700

    Interception

--- cypress/e2e/rewards.spec.js
+++ cypress/e2e/rewards.spec.js
@@ -5,4 +5,10 @@ describe("Rewards dashboard", () => {
   it("should render the UI", () => {
     rewardsUI();
   });
+  it("Network testing", () => {
+    cy.visit("/rewards");
+    cy.intercept("GET", "/rewards", (res) => {
+      cy.log(res.body);
+    });
+  });
 });
