c6537c63b988efca53a7d523bdeb1b14be331f5a
putrasatr <satriaputrapratama2002@gmail.com>
2022-03-07 09:51:58 +0700

    File Upload and generic html

---
+++ src/components/FileUpload/index.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import { FileUploader } from "react-drag-drop-files";
+import useLocalHooks from "../../hooks";
+
+const FileUp = () => {
+  const { useDebounce } = useLocalHooks(); // never[] ;
+  const fetching = () => {
+    fetch;
+    return () => {};
+  };
+  useEffect(() => {
+      
+  }, []);
+};
---
+++ src/hooks/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const useLocalHooks = () => {
+  const useDebounce = () => {
+    return "Hello";
+  };
+  return { useDebounce };
+};
+
+export default useLocalHooks;
